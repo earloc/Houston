f997d9e23bfcc68d5ae6c43b1e51c2a577d92eef
earloc <earloc@live.de>
2021-01-02 01:33:50 +0100

    prepare more tests

--- src/Houston.Tests/Integrations/Audio/SetVolumeCommandTests.cs
+++ src/Houston.Tests/Integrations/Audio/SetVolumeCommandTests.cs
@@ -33,5 +33,38 @@ namespace Houston.Tests.Integrations.Audio
             actual.Should().Be(expected, "that´s what should be accepted and adopted");
         }
 
+        [Theory]
+        [InlineData(100)]
+        [InlineData(110)]
+        [InlineData(125)]
+        [InlineData(150)]
+        [InlineData(175)]
+        [InlineData(1000)]
+        public async Task Limits_Values_Exceeding_Default_UpperBounds(int value)
+        {
+            var response = await fixture.Mediator.Send(new SetVolumeCommand.Request(value));
+            var actual = response.Volume;
+
+            var expected = 100;
+
+            actual.Should().Be(expected, "this denotes the maximum upper bound value");
+        }
+
+        [Theory]
+        [InlineData(-0)]
+        [InlineData(-10)]
+        [InlineData(-25)]
+        [InlineData(-50)]
+        [InlineData(-75)]
+        [InlineData(-100)]
+        public async Task Limits_Values_Exceeding_Default_LowerBounds(int value)
+        {
+            var response = await fixture.Mediator.Send(new SetVolumeCommand.Request(value));
+            var actual = response.Volume;
+
+            var expected = 0;
+
+            actual.Should().Be(expected, "this denotes the minimum lower bound value");
+        }
     }
 }
